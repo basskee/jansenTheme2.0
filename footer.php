5f93e769f9c4f72f9cea27ba1e1422cd46a52bc5
Blake DeBaske <blakedebaske@gmail.com>
2016-04-28 19:21:43 -0400

    properly enqueued JQuery and added file structure

--- footer.php
+++ footer.php
@@ -1,24 +1,11 @@
-<div class="site-footer">
-	<p class="copyrights">
-		Jansen <span style="font-size:15px; font-family: sans-serif;">&copy;</span>
-	</p>
-	<div class="footer-menu">
-		<?php wp_nav_menu(array("theme_location" => "footer-menu") );?>
-	</div>
-</div>
+<?php
 
-</div><!--  End of Container -->
+	/*
+		This is the template for the header
 
-
-<script type="text/javascript">
-
-	// Can also be used with $(document).ready()
-	jQuery(window).load(function() {
-	  jQuery('.flexslider').flexslider({
-		animation: "slide"
-	  });
-	});
-</script>
+		@package jansentheme
+	 */
+?>
 
 <?php wp_footer(); ?>
 </body>
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -33,3 +33,22 @@ function jansen_load_admin_scripts( $hook ){
 add_action('admin_enqueue_scripts', 'jansen_load_admin_scripts');
 
 
+/*
+	====================================
+	FRONT-END ENQUEUE FUNCTIONS
+	====================================
+*/
+
+function jansen_load_scripts(){
+
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0', 'all');
+	wp_enqueue_style( 'jansen', get_template_directory_uri() . '/css/jansen.css', array(), '1.0.0', 'all');
+
+	wp_deregister_script( 'jquery');
+	wp_register_script('jquery', get_template_directory_uri() . '/js/jquery-1.12.3.min.js', false, '1.12.3', true );
+	wp_enqueue_script('jquery' );
+
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.0.0', 'all');
+
+}
+add_action( 'wp_enqueue_scripts', 'jansen_load_scripts' );
